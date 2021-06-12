840a832f5d6a1037008f508718edb3a2144b69aa
corazonsky <jamesnicholass16@gmail.com>
2021-06-13 01:45:09 +0700

    set up project for firestore backend

---
+++ lib/models/User.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  String uid;
+  String email;
+  String displayName;
+  String phoneNumber;
+  String photoUrl;
+  String description;
+
+  UserModel(
+      {this.uid,
+      this.email,
+      this.displayName,
+      this.phoneNumber,
+      this.photoUrl});
+}
